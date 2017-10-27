ec330e4fd7395da3f4f4e7257e8193e93c811c7e
manelgavalda <expedientmanel@gmail.com>
2017-10-27 22:00:36 +0200

    Added Provider on DOM

--- src/client/client.js
+++ src/client/client.js
@@ -2,11 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import Router from './Routes';
 
+const store = createStore(reducers, {}, applyMiddleware(thunk))
+
 ReactDOM.hydrate(
+  <Provider store={store}>
   <BrowserRouter>
     <Router />
-  </BrowserRouter>,
+  </BrowserRouter>
+</Provider>,
   document.querySelector('#root')
 );
