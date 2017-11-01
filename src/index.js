b4c8f2934b1944f173c6cd6e94e5fe5762900bc4
manelgavalda <expedientmanel@gmail.com>
2017-11-01 13:40:11 +0100

    Added admins action creator and reducer

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ app.get('*', (req, res) => {
 
   Promise.all(promises).then(() => {
     const context = {};
-    const content = renderer(req, store, context)
+    const content = renderer(req, store, context);
 
     if(context.notFound) {
       res.status(404)
